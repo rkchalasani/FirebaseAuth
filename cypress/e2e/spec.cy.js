a9dc13f22243ad3b3292f75afa8cac1bc00787bb
rkchalasani <rkchalasani.99@gmail.com>
2022-06-28 14:11:54 +0530

    made changes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,11 @@
 describe("empty spec", () => {
   it("passes", () => {
     cy.visit("http://localhost:3000/");
-    cy.get(".underline").click();
-    cy.get("#signupemail").type("abcd@abcd.com");
-    cy.get("#signuppass").type("123456");
-    cy.get("#signup").click();
-    cy.get('#signin').click()
+    // cy.get(".underline").click();
+    // cy.get("#signupemail").type("abcd@abcd.com");
+    // cy.get("#signuppass").type("123456");
+    // cy.get("#signup").click();
+    // cy.get('#signin').click();
     cy.get("#input-email").type("abcd@abcd.com");
     cy.get("#input-pass").type("123456");
     cy.get("#loginbutton").click();
